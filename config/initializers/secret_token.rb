3a473405695951b216c9012824debabd2dd9aa48
jvii05 <jvsc_05@hotmail.com>
2012-10-04 13:48:42 -0600

    Subo la estructura

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabAgenda::Application.config.secret_token = '34b658e3ed5fdfb18f5d2086450ee6338982df474ee1d08ff24027ce281e9e41c062864a5d22304829b9ca70eac2e8371b93903c4fb0aad1fefb89a45ebec3a3'
